078c3459cd2fee9a8612b3580d8f3a0ab65a8536
TheBlackPharaon <59323204+TheBlackPharaon@users.noreply.github.com>
2021-02-22 22:57:42 +0000

    master

--- Design/JS/style.js
+++ Design/JS/style.js
@@ -49,15 +49,3 @@ $(function () {
 
 
 });
-
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementsByClassName("navbar0").style.top = "0";
-  } else {
-    document.getElementsByClassName("navbar0").style.top = "-50px";
-  }
-  prevScrollpos = currentScrollPos;
-}
