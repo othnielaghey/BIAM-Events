a9822f49b58c7101d79d2ff86f875851f1981e80
TheBlackPharaon <othnielignacio@gmail.com>
2021-03-06 11:42:25 +0000

    master

--- Design/JS/index.js
+++ Design/JS/index.js
@@ -1,15 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
 // Get the modal
 var modal1 = document.getElementById("whatwedo1");
 var modal2 = document.getElementById("whatwedo2");
@@ -23,16 +11,16 @@ var btn3 = document.getElementById("myBtn3");
 var btn4 = document.getElementById("myBtn4");
 
 // When the user clicks on the button, open the modal
-btn1.onclick = function() {
+btn1.onclick = function () {
   modal1.style.display = "block";
 }
-btn2.onclick = function() {
+btn2.onclick = function () {
   modal2.style.display = "block";
 }
-btn3.onclick = function() {
+btn3.onclick = function () {
   modal3.style.display = "block";
 }
-btn4.onclick = function() {
+btn4.onclick = function () {
   modal4.style.display = "block";
 }
 
@@ -43,21 +31,21 @@ var close3 = document.getElementById("close3");
 var close4 = document.getElementById("close4");
 
 // When the user clicks on <span> (x), close the modal
-close1.onclick = function() {
+close1.onclick = function () {
   modal1.style.display = "none";
 }
-close2.onclick = function() {
+close2.onclick = function () {
   modal2.style.display = "none";
 }
-close3.onclick = function() {
+close3.onclick = function () {
   modal3.style.display = "none";
 }
-close4.onclick = function() {
+close4.onclick = function () {
   modal4.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal1 | event.target == modal2 | event.target == modal3 | event.target == modal4) {
     modal1.style.display = "none";
     modal2.style.display = "none";
--- Design/JS/style.js
+++ Design/JS/style.js
@@ -2,38 +2,38 @@
 let themeMode = sessionStorage.getItem("themeMode");
 
 $("#dark-toggle").click(
-  function(){
-  $('body').removeClass('light');
-  $('body').addClass('dark');
+  function () {
+    $('body').removeClass('light');
+    $('body').addClass('dark');
 
-  sessionStorage.setItem("themeMode", 'dark');
+    sessionStorage.setItem("themeMode", 'dark');
 
-})
+  })
 
 $("#light-toggle").click(
-  function(){
-  $('body').removeClass('dark');
-  $('body').addClass('light');
+  function () {
+    $('body').removeClass('dark');
+    $('body').addClass('light');
 
-  sessionStorage.setItem("themeMode", 'light');
-
-})
-
-if(themeMode == null){
-  sessionStorage.setItem("themeMode", 'dark'); //set default themeMode
-  }
-  
-if(themeMode == 'light'){
-  $('body').addClass('light');
-  $('body').removeClass('dark');
     sessionStorage.setItem("themeMode", 'light');
+
+  })
+
+if (themeMode == null) {
+  sessionStorage.setItem("themeMode", 'dark'); //set default themeMode
+}
+
+if (themeMode == 'light') {
+  $('body').addClass('light');
+  $('body').removeClass('dark');
+  sessionStorage.setItem("themeMode", 'light');
 
 }
 
-if(themeMode == 'dark'){
+if (themeMode == 'dark') {
   $('body').addClass('dark');
   $('body').removeClass('light');
-    sessionStorage.setItem("themeMode", 'dark');
+  sessionStorage.setItem("themeMode", 'dark');
 
 }
 
@@ -41,11 +41,11 @@ if(themeMode == 'dark'){
 //document ready
 $(function () {
 
-    //if mobile burger menu clicked
-    $('#toggle').click(function () {
-        $(this).toggleClass('active');
-        $('#overlay').toggleClass('open');
-    });
+  //if mobile burger menu clicked
+  $('#toggle').click(function () {
+    $(this).toggleClass('active');
+    $('#overlay').toggleClass('open');
+  });
 
 
 });
